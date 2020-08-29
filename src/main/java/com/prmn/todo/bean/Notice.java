ed2a7faab1a8e8173816f8176778834dfb33ec34
snow-01 <cist.b218.t.mathumoto@gmail.com>
2020-08-29 11:16:04 +0900

    8/29 11:15

--- src/main/java/com/prmn/todo/bean/Notice.java
+++ src/main/java/com/prmn/todo/bean/Notice.java
@@ -5,4 +5,43 @@ public class Notice {
     private String accountId;
     private boolean todoConfiguration;
     private long dueDate;
+
+    public Notice(long id, String accountId, boolean todoConfiguration, long dueDate) {
+        this.id = id;
+        this.accountId = accountId;
+        this.todoConfiguration = todoConfiguration;
+        this.dueDate = dueDate;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(String accountId) {
+        this.accountId = accountId;
+    }
+
+    public boolean isTodoConfiguration() {
+        return todoConfiguration;
+    }
+
+    public void setTodoConfiguration(boolean todoConfiguration) {
+        this.todoConfiguration = todoConfiguration;
+    }
+
+    public long getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(long dueDate) {
+        this.dueDate = dueDate;
+    }
 }
