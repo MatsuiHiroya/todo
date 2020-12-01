24df8d331ff234bd6824909493605d495c5436c5
Matsui Hiroya <dmo4649@gmail.com>
2020-12-01 19:14:48 +0900

    12月1日7時13分 CreateToDoPageとToDoListPageの変更 pomにDatePickerの依存関係を追加

--- src/main/java/com/prmn/todo/page/ToDoListPage.java
+++ src/main/java/com/prmn/todo/page/ToDoListPage.java
@@ -4,25 +4,31 @@ import com.prmn.todo.bean.ToDo;
 import com.prmn.todo.service.IToDoListPageService;
 import com.prmn.todo.service.IToDoPageService;
 import com.prmn.todo.service.ToDoListPageService;
+import javafx.scene.control.RadioButton;
+import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.AjaxLink;
 import org.apache.wicket.ajax.markup.html.form.AjaxButton;
+import org.apache.wicket.ajax.markup.html.form.AjaxCheckBox;
 import org.apache.wicket.behavior.AttributeAppender;
+import org.apache.wicket.markup.html.TransparentWebMarkupContainer;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.markup.html.form.Button;
-import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.*;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.markup.html.link.ExternalLink;
 import org.apache.wicket.markup.html.link.PopupSettings;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
+import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.wicketstuff.annotation.mount.MountPath;
 
 import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @MountPath("ToDoList")
@@ -50,12 +56,20 @@ public class ToDoListPage extends WebPage {
             }
         });
 
-
-
         WebMarkupContainer todoListWMC = new WebMarkupContainer("todoListWMC");
         todoListWMC.setOutputMarkupId(true);
         add(todoListWMC);
 
+        var deleteBulkCheckBox = new AjaxCheckBox("deleteBulkCheckBox",Model.of(Boolean.TRUE)){
+            @Override
+            public void onUpdate(AjaxRequestTarget target){
+                target.add(todoListWMC);
+            }
+        };
+
+        //var deleteBulkCheckBox = new CheckBox("deleteBulkCheckBox");
+
+        var cssModel = Model.of("background-color :#98fb98;");
 
         // Service からデータベースのユーザ一覧をもらい、Modelにする
         // List型のモデルは Model.ofList(...) で作成する。
@@ -105,25 +119,76 @@ public class ToDoListPage extends WebPage {
                 var deleteToDoForm = new Form<>("deleteToDoForm");
                 listItem.add(deleteToDoForm);
 
+
+
+                deleteBulkCheckBox.setOutputMarkupPlaceholderTag(true);
+                //deleteBulkCheckBox.setVisible(false);
+
                 AjaxButton ajaxButton = new AjaxButton("deleteToDoButton"){
                     @Override
                     public void onSubmit(AjaxRequestTarget target){
                         super.onSubmit(target);
                         toDoListPageService.deleteToDoList(toDo.getId());
                         //実際には、selectToDoListの引数にセッションからのaccount_idを渡す
+                        cssModel.setObject("background-color :#98fb98;");
                         todoListModel.setObject(toDoListPageService.selectToDoList());
                         target.add(todoListWMC);
                     }
                 };
                 deleteToDoForm.add(ajaxButton);
 
+                String s = "1fgg";
+                char ss = s.charAt(0);
+                int sss = ss;
 
+                listItem.add(deleteBulkCheckBox);
+
+                //if(deleteBulkCheckBox.getConvertedInput() == false){
+                    listItem.add(AttributeModifier.replace("class", "todoList2"));
+                //}
 
             }
 
         };
         todoLV.setOutputMarkupId(true);
         todoListWMC.add(todoLV);
+
+        TransparentWebMarkupContainer bodyContainer = new TransparentWebMarkupContainer("body");
+        bodyContainer.add(AttributeModifier.replace("css", cssModel));
+
+        var deleteBulkFormWMC = new WebMarkupContainer("deleteBulkFormWMC");
+        deleteBulkFormWMC.setOutputMarkupId(true);
+        add(deleteBulkFormWMC);
+
+        var deleteToDoBulkForm = new Form<>("deleteToDoBulkForm");
+        deleteBulkFormWMC.add(deleteToDoBulkForm);
+
+        var cancelButton = new AjaxButton("cancelButton"){
+            @Override
+            public void onSubmit(AjaxRequestTarget target){
+                this.setVisible(false);
+                deleteBulkCheckBox.setVisible(false);
+                deleteBulkCheckBox.setVisible(false);
+
+                target.add(todoListWMC,deleteBulkFormWMC);
+            }
+        };
+        deleteToDoBulkForm.add(cancelButton);
+        cancelButton.setOutputMarkupPlaceholderTag(true);
+        cancelButton.setVisible(false);
+
+        var deleteToDoBulkButton = new AjaxButton("deleteToDoBulkButton"){
+            @Override
+            public void onSubmit(AjaxRequestTarget target){
+                super.onSubmit();
+                cancelButton.setVisible(true);
+                deleteBulkCheckBox.setVisible(true);
+                target.add(todoListWMC,deleteBulkFormWMC);
+            }
+        };
+        deleteToDoBulkForm.add(deleteToDoBulkButton);
+
+
 
 /**
         var editToDoForm = new Form<>("editToDoForm");
@@ -144,4 +209,6 @@ public class ToDoListPage extends WebPage {
 
 
     }
+
+
 }
