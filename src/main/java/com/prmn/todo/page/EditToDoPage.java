c0826125e758bb480356eb4c05aeaae9a55f9aea
MasakiYoshizawa <cist.b218.m.yosizawa@gmail.com>
2020-12-22 12:42:29 +0900

    2020/12/22 EditToDoがTimestampに対応したよ

--- src/main/java/com/prmn/todo/page/EditToDoPage.java
+++ src/main/java/com/prmn/todo/page/EditToDoPage.java
@@ -4,17 +4,16 @@ import com.prmn.todo.bean.ToDo;
 import com.prmn.todo.service.IEditToDoPageService;
 import org.apache.wicket.extensions.markup.html.form.DateTextField;
 import org.apache.wicket.markup.html.WebPage;
-import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.*;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.markup.html.link.Link;
-import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.wicketstuff.annotation.mount.MountPath;
 import org.wicketstuff.jwicket.ui.datepicker.DatePicker;
 
 import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
@@ -58,14 +57,27 @@ public class EditToDoPage extends WebPage {
         editForm.add(date);
         date.add(new DatePicker());
 
+        String currentTimestampToString = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(toDo.getLimitTime());
+        var limitHour = currentTimestampToString.substring(11,13);
+        var limitMinute = currentTimestampToString.substring(14,16);
+
+        List<String> limitHourList = new ArrayList<>();
+        for(int i=0;i<24;i++)limitHourList.add(Integer.toString(i));
+        var hourDropdown = new DropDownChoice("hour",new Model(limitHour),limitHourList);
+        editForm.add(hourDropdown);
+
+        List<String> limitMinuteList = new ArrayList<>();
+        for(int i=0;i<60;i+=5)limitMinuteList.add(Integer.toString(i));
+        DropDownChoice<String> minuteDropdown = new DropDownChoice("minute",new Model(limitMinute),limitMinuteList);
+        editForm.add(minuteDropdown);
+
         List<String> typeDropdown = new ArrayList<>(){};
-        typeDropdown.addAll(Arrays.asList("講義","アンケート"));
+        typeDropdown.addAll(Arrays.asList("講義","アンケート","その他"));
         DropDownChoice<String> dropDownChoice = new DropDownChoice("type",new Model(toDo.getType()),typeDropdown);
         editForm.add(dropDownChoice);
 
         var todoName = new TextField("todoName",new Model<>(toDo.getTodoName()));
         editForm.add(todoName);
-
 
         var todoContent = new TextArea("todoContent",new Model<>(toDo.getTodoContent()));
         editForm.add(todoContent);
@@ -74,8 +86,12 @@ public class EditToDoPage extends WebPage {
             @Override
             public void onSubmit() {
                 try {
-                    editToDoPageService.updateToDo(todoName.getModelObject().toString(), todoContent.getModelObject().toString(), new Timestamp(date.getModelObject().getTime()), dropDownChoice.getModelObject(),todoId);
-                    setResponsePage(ToDoPage.class);
+                    String limitDateToString = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(date.getModelObject()).substring(0,10);
+                    String limit = limitDateToString + " " + hourDropdown.getModelObject() + ":" + minuteDropdown.getModelObject() + ":00";
+                    Timestamp ts = new Timestamp(new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").parse(limit).getTime());
+
+                    editToDoPageService.updateToDo(todoName.getModelObject().toString(), todoContent.getModelObject().toString(), ts, dropDownChoice.getModelObject(),todoId);
+                    setResponsePage(ToDoListPage.class);
                 }catch (Exception e){
                     System.out.println(e);
                 }
